48cd0798f8cd93ac96c2edb25c20e7ebcb9f2eed
root <root@php-dev.home>
2016-08-14 22:00:58 -0400

    Fixed all errors

--- hello.php
+++ hello.php
@@ -1,13 +1,18 @@
-<? php
+<html>
+<body>
 
-echo '<h1 style="color:green"> Hello World, THis is robo </h1>';
+<?php
+
+echo '<h1> Hello World, THis is robo </h1>';
 
 for ($i=0; $i<100; $i++ ){
   if ($i % 2 == 0 ) {
-    echo "<span style='color:green'>$i is Even number</span>";
+    echo "<span style='color:green'>$i is Even number</span> <br>";
   }else {
-    echo "<span style='color:red'>$i is odd number</span>";  
+    echo "<span style='color:red'>$i is odd number</span><br>";  
   }
 }
 
 ?>
+</body>
+</html>
