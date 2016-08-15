387598b5297dae423c327f082843ef28069ac4e5
yvsp <usyvsp@gmail.com>
2016-08-14 18:51:09 -0700

    initial commit

---
+++ hello.php
@@ -0,0 +1,13 @@
+<? php
+
+echo '<h1 style="color:green"> Hello World, THis is robo </h1>'
+
+for ($i=0; $<100; $i++ ){
+  if ($i % 2 == 0 ) {
+    echo "<span style='color:green'>$i is Even number</span>";
+  }else {
+    echo "<span style='color:red'>$i is odd number</span>";  
+  }
+}
+
+?>
